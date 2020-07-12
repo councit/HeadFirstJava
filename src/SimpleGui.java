68e6f176b9fc360c704161a2bf6db8e514a24fb5
councit <councit@gmail.com>
2020-07-12 16:51:11 -0700

    gui created

---
+++ src/SimpleGui.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class SimpleGui {
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click Me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+
+    }
+}
